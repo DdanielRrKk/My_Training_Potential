7ee65b0f89819389948e649ec9f2901d911c252c
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2022-03-23 14:45:31 +0200

    Delete database/services directory

--- database/services/user_services/user_services.js
+++
@@ -1,3 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { USER_MEALS_STORE } from "../../database_stores";
-import { USER_MEALS_SHEMA } from '../../database_shemas';
