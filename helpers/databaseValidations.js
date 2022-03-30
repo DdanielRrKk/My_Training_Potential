ddcb162ffdca4957865deb1bcf902278519e9467
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2022-03-30 14:15:19 +0300

    Delete databaseValidations.js

--- helpers/databaseValidations.js
+++
@@ -1,7 +0,0 @@
-export function IsResultEmpty(result) {
-    return (result == null || result == '' || result == []);
-}
-
-export function IsFlagCorrect(flag) {
-    return (!flag || flag === null || flag === undefined || flag === '' || flag === 'NaN');
-}
