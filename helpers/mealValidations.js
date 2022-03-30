b6d469cecd47f8f17e9e730cb125a1db28fcbbdd
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2022-03-30 14:15:31 +0300

    Delete mealValidations.js

--- helpers/mealValidations.js
+++
@@ -1,8 +0,0 @@
-export function CheckIsCorrectMealInput(input) {
-    return (input == null || input == '' || input == 0) ? false : true;
-}
-
-export function GetCorrectMealInput(input, isInc) {
-    if(isInc) return (input == null || input == '' || input <= 0 || input == 'NaN') ? 1 : (parseInt(input) + 1);
-    return (input == null || input == '' || input <= 0 || input == 'NaN') ? 0 : (parseInt(input) - 1);
-}
