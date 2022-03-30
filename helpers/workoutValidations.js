ca9f766ad3efac8c3f19e18aecd9cdbaece40dd1
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2022-03-30 14:15:40 +0300

    Delete workoutValidations.js

--- helpers/workoutValidations.js
+++
@@ -1,8 +0,0 @@
-export function CheckIsCorrectWorkoutInput(input) {
-    return (input == null || input == '' || input == 0) ? false : true;
-}
-
-export function GetCorrectWorkoutInput(input, isInc) {
-    if(isInc) return (input == null || input == '' || input <= 0 || input == 'NaN') ? 1 : (parseInt(input) + 1);
-    return (input == null || input == '' || input <= 0 || input == 'NaN') ? 0 : (parseInt(input) - 1);
-}
